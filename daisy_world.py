8f36743fe81024dc36bb5f066c90415bdcf91f27
Ben Johnson <34067866+benwjohnson@users.noreply.github.com>
2023-02-05 15:09:43 -0600

    new edit bwj

---
+++ daisy_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 14:21:33 2023
+
+@author: bwj
+"""
+
+#File for working on our Daisy World Model. We can try to use a github repository to all 
+#work on this together! We'll use the equations from Watson and Lovelock 1983. 
+
+import numpy as np
+from scipy.integrate import odeint
+
+#test edit 
+#second edit
